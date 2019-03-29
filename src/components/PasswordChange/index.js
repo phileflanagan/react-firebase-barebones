39ed3d564cf643a034b474be37e09b06f070cd7c
Phil Flanagan <phileflanagan@gmail.com>
2019-03-29 14:28:00 -0400

    Implement basic authorization

---
+++ src/components/PasswordChange/index.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+
+import { withFirebase } from '../Firebase';
+
+const INITIAL_STATE = {
+    passwordOne: '',
+    passwordTwo: '',
+    error: null
+}
+
+class PasswordChangeForm extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { ...INITIAL_STATE };
+    }
+
+    onSubmit = e => {
+        const { passwordOne } = this.state;
+        this.props.firebase
+            .doPasswordUpdate(passwordOne)
+            .then(()=> {
+                this.setState({ ...INITIAL_STATE });
+            })
+            .catch(error => {
+                this.setState({ error });
+            });
+
+        e.preventDefault();
+    }
+
+    onChange = e => {
+        this.setState({ [e.target.name]: e.target.value });
+    }
+
+    render() {
+        const { passwordOne, passwordTwo, error } = this.state;
+        const isInvalid = passwordOne !== passwordTwo || passwordOne === '';
+        return (
+            <form onSubmit={this.onSubmit}>
+                <input
+                    name="passwordOne"
+                    value={passwordOne}
+                    type="password"
+                    placeholder="New Password"
+                />
+                <input
+                    name="passwordTwo"
+                    value={passwordTwo}
+                    type="password"
+                    placeholder="Confirm New Password"
+                />
+                <button disabled={isInvalid} type="submit">Update Password</button>
+
+                {error && <p>{error.message}</p>}
+            </form>
+        );
+    }
+}
+
+export default withFirebase(PasswordChangeForm);
