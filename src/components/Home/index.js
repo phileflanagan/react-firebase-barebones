aecf99f88d206c1864f3c8f367fa91d6e0f63da6
Phil Flanagan <phileflanagan@gmail.com>
2019-04-01 17:58:39 -0400

    Add pagination, and user assocation to messages

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -4,13 +4,42 @@ import { compose } from 'recompose';
 import { AuthUserContext, withAuthorization, withEmailVerification } from '../Session';
 import { withFirebase } from '../Firebase';
 
-const HomePage = () => (
-    <div>
-        <h1>Home</h1>
-        <p>This page is accessible by every signed in user.</p>
-        <Messages />
-    </div>
-);
+class HomePage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            users: null
+        }
+    }
+
+    componentDidMount() {
+        this.unsubscribe = this.props.firebase
+            .users()
+            .on('value', snapshot => {
+                // let users = {};
+                console.log("SNAPSHOT", snapshot)
+                // snapshot.forEach(doc => (users[doc.id] = doc.data()));
+        
+                this.setState({
+                    users: snapshot.val()
+                });
+            });
+    }
+
+    componentWillUnmount() {
+        this.unsubscribe();
+    }
+
+    render() {
+        return (
+        <div>
+            <h1>Home</h1>
+            <p>This page is accessible by every signed in user.</p>
+            <Messages users={this.state.users} />
+        </div>
+        );
+    }
+}
 
 class MessagesBase extends Component {
     constructor(props) {
@@ -18,31 +47,54 @@ class MessagesBase extends Component {
         this.state = {
             text: '',
             loading: false,
-            messages: []
+            messages: [],
+            limit: 5,
+            endOfMessagesReached: false,
         }
     }
 
     componentDidMount() {
-        this.setState({ loading: true });
-
-        this.props.firebase.messages().on('value', snapshot => {
-            const messageObject = snapshot.val();
-
-            if (messageObject) {
-                const messageList = Object.keys(messageObject).map(key => ({
-                    ...messageObject[key],
-                    uid: key
-                }));
-                this.setState({ messages: messageList, loading: false});   
-            } else {
-                this.setState({ messages: null, loading: false});
-            }
-        })
+        this.onListenForMessages();
     }
 
     componentWillUnmount() {
         this.props.firebase.messages().off();
     }
+
+    onListenForMessages() {
+        this.setState({ loading: true });
+        const oldMessageLength = this.state.messages.length;
+        console.log('oldmessagelength', oldMessageLength)
+        this.props.firebase
+            .messages()
+            .orderByChild('createdAt')
+            .limitToLast(this.state.limit)
+            .on('value', snapshot => {
+                const messageObject = snapshot.val();
+
+                if (messageObject) {
+                    const messageList = Object.keys(messageObject).map(key => ({
+                        ...messageObject[key],
+                        uid: key
+                    }));
+
+                    const endOfMessagesReached = (oldMessageLength === messageList.length || messageList.length < this.state.limit)
+                    
+                    this.setState({ 
+                        messages: messageList, 
+                        loading: false,
+                        endOfMessagesReached
+                    });   
+                } else {
+                    this.setState({ messages: null, loading: false});
+                }
+            })
+    }
+
+    onNextPage = () => {
+        this.setState(state => ({limit: state.limit + 5}), this.onListenForMessages);
+    }
+
 
     onChangeText = e => {
         this.setState({ text: e.target.value})
@@ -76,22 +128,31 @@ class MessagesBase extends Component {
     }
 
     render() {
-        const { text, messages, loading } = this.state;
+        const { users } = this.props;
+        const { text, messages, loading, endOfMessagesReached } = this.state;
         return (
             <AuthUserContext.Consumer>
                 {authUser => (
                     <div>
                         {loading && <div>Loading...</div>}
-                        {messages ? (
+                        {!loading && messages && !endOfMessagesReached && (
+                            <button type="button" onClick={this.onNextPage}>
+                                More
+                            </button>
+                        )}
+                        {messages && (
                             <MessageList 
-                                messages={messages}
+                                messages={messages.map(message => ({
+                                    ...message,
+                                    user: users
+                                      ? users[message.userId]
+                                      : { userId: message.userId },
+                                  }))}
                                 onEditMessage={this.onEditMessage}
                                 onRemoveMessage={this.onRemoveMessage} 
                             />
-                        ) : (
-                            <div>There are no messages...</div>
                         )}
-
+                        {!messages && <div>There are no messages...</div>}
                         <form onSubmit={e => this.onCreateMessage(e, authUser)}>
                             <input 
                                 type="text"
@@ -148,6 +209,7 @@ class MessageItem extends Component {
     render() {
         const { message, onRemoveMessage } = this.props;
         const { editMode, editText } = this.state;
+        console.log("Messages", message)
         return (
             <li>
                 {editMode ? (
@@ -158,7 +220,7 @@ class MessageItem extends Component {
                     />
                 ) : (
                     <span>
-                        <strong>{message.userId}</strong> {message.text}
+                        <strong>{message.user.username || message.user.userId}</strong> {message.text}
                         {message.modified && <small> (edited)</small>}
                     </span>
                 )}
@@ -183,6 +245,7 @@ const Messages = withFirebase(MessagesBase);
 const condition = authUser => !!authUser;
 
 export default compose(
+    withFirebase,
     withEmailVerification,
     withAuthorization(condition)
 )(HomePage)
